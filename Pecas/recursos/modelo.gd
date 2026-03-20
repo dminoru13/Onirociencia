54e2a40bee6aac37cfd15681a723ca023b7e4ed5
notridani <musicodorpg@gmail.com>
2026-03-20 13:50:10 -0300

    retiro tudo que eu disse, deu tudo certo com o reactive

--- Pecas/recursos/modelo.gd
+++ Pecas/recursos/modelo.gd
@@ -5,9 +5,14 @@ extends Reactive
 @export var caminho_modelo: String:
 	set(v):
 		caminho_modelo = v
-		print("(MODELO) mudou o caminho")
+		print("(MODELO) ", nome_reactive, " mudou o caminho")
+		print("(MODELO) o meu dono é: ", owner.nome_reactive)
 		reactive_changed.emit()
 @export var posicao: Vector3 = Vector3(0,0,0)
 @export var key_shape: float
 @export var lista_modificador: ArrayMelhor = ArrayMelhor.new("lista_modificador", self)
 @export var lista_modificador_universal: ArrayMelhor = ArrayMelhor.new("lista_modificador_universal", self)
+
+func _init(nome = null, initial_owner: Reactive = null) -> void:
+	super._init(initial_owner)
+	
