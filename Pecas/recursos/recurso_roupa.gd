2abf94811d67a010e4d890d1b1a9916815d64cfd
notridani <musicodorpg@gmail.com>
2026-03-25 12:25:56 -0300

    criar roupas é dificil

--- Pecas/recursos/recurso_roupa.gd
+++ Pecas/recursos/recurso_roupa.gd
@@ -1,3 +1,8 @@
 @tool
 class_name RecursoRoupa
 extends Resource
+
+@export var nome: String
+@export var tipo: String
+@export var esconder_parte_do_corpo: bool
+@export var dicionario_modelos: Dictionary[String, Modelo]
